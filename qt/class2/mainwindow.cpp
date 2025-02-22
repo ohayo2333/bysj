794dccd5931231d40ad565024c4ce1fcb2bcf02e
ohayo2333 <sha30649@gmail.com>
2025-02-22 21:47:18 +0800

    250222

--- qt/class2/mainwindow.cpp
+++ qt/class2/mainwindow.cpp
@@ -12,3 +12,67 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_airConditionerBtn_clicked()
+{
+    AirConditioner *dialog = new AirConditioner(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_curtainBtn_clicked()
+{
+    Curtain *dialog = new Curtain(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_lightBtn_clicked()
+{
+    Light *dialog = new Light(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_environmentBtn_clicked()
+{
+    Environment *dialog = new Environment(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_lockBtn_clicked()
+{
+    Lock *dialog = new Lock(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_powerSystemBtn_clicked()
+{
+    PowerSystem *dialog = new PowerSystem(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_gpsBtn_clicked()
+{
+    GPS *dialog = new GPS(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+void MainWindow::on_securityBtn_clicked()
+{
+    Security *dialog = new Security(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
+
+
+void MainWindow::on_dashboardBtn_clicked()
+{
+    Dashboard *dialog = new Dashboard(this);
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog->show();
+}
--- qt/class2/mainwindow.h
+++ qt/class2/mainwindow.h
@@ -2,10 +2,21 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include"airconditioner.h"
+#include"curtain.h"
+#include"light.h"
+#include"environment.h"
+#include"lock.h"
+#include"powersystem.h"
+#include"gps.h"
+#include"security.h"
+#include"dashboard.h"
 
+QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
@@ -14,6 +25,25 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_airConditionerBtn_clicked();
+
+    void on_curtainBtn_clicked();
+
+    void on_lightBtn_clicked();
+
+    void on_environmentBtn_clicked();
+
+    void on_lockBtn_clicked();
+
+    void on_powerSystemBtn_clicked();
+
+    void on_gpsBtn_clicked();
+
+    void on_securityBtn_clicked();
+
+    void on_dashboardBtn_clicked();
 
 private:
     Ui::MainWindow *ui;
